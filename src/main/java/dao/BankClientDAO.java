4af1c2992a6b279aa61867a08e71f7c89e9aa11d
rustamg070619 <61186077+rustamg070619@users.noreply.github.com>
2020-03-03 17:57:06 +0300

    commit 2

--- src/main/java/dao/BankClientDAO.java
+++ src/main/java/dao/BankClientDAO.java
@@ -3,10 +3,7 @@ package dao;
 import exception.DBException;
 import model.BankClient;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -48,23 +45,26 @@ public class BankClientDAO {
         if (validateClient(name, password)) {
             if (isClientHasSum(name, transactValue)) {
                 long after = getClientByName(name).getMoney() - transactValue;
-                Statement stmt = connection.createStatement();
-                stmt.executeUpdate("UPDATE bank_client SET money =" + after + " WHERE name = '" + name + "'");
-                // stmt.close();
+                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE bank_client SET money = (?) WHERE name = (?)");
+                preparedStatement.setLong(1, after);
+                preparedStatement.setString(2, name);
+                preparedStatement.executeUpdate();
+                preparedStatement.close();
             } else throw new SQLException();
         } else throw new SQLException();
     }
 
     public BankClient getClientById(long id) throws SQLException {
-        Statement stmt = connection.createStatement();
-        stmt.executeQuery("SELECT * FROM bank_client WHERE name ='" + id + "'");
-        ResultSet resultSet = stmt.getResultSet();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM bank_client WHERE name = (?)");
+        preparedStatement.setLong(1, id);
+        preparedStatement.executeQuery();
+        ResultSet resultSet = preparedStatement.getResultSet();
         resultSet.next();
         long idResult = resultSet.getLong(1);
         String nameClient = resultSet.getString(2);
         String password = resultSet.getString(3);
         Long money = resultSet.getLong(4);
-        stmt.close();
+        preparedStatement.close();
         return new BankClient(idResult, nameClient, password, money);
 
     }
@@ -74,44 +74,49 @@ public class BankClientDAO {
     }
 
     public long getClientIdByName(String name) throws SQLException {
-        Statement stmt = connection.createStatement();
-        stmt.execute("select * from bank_clien where name='" + name + "'");
-        ResultSet result = stmt.getResultSet();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from bank_clien WHERE name = (?)");
+        preparedStatement.setString(1, name);
+        preparedStatement.execute();
+        ResultSet result = preparedStatement.getResultSet();
         result.next();
         long id = result.getLong(1);
         result.close();
-        stmt.close();
+        preparedStatement.close();
         return id;
     }
 
     public BankClient getClientByName(String name) throws SQLException {
-        Statement stmt = connection.createStatement();
-        stmt.executeQuery("SELECT * FROM bank_client WHERE name ='" + name + "'");
-        ResultSet result = stmt.getResultSet();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM bank_client WHERE name = (?)");
+        preparedStatement.setString(1, name);
+        preparedStatement.executeQuery();
+        ResultSet result = preparedStatement.getResultSet();
         result.next();
         String nameClient = result.getString(2);
         long id = result.getLong(1);
         String password = result.getString(3);
         Long money = result.getLong(4);
-        stmt.close();
+        preparedStatement.close();
         return new BankClient(id, nameClient, password, money);
     }
 
     public void addClient(BankClient client) throws SQLException {
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("INSERT INTO bank_client(name, password, money) VALUES ( '" + client.getName() + "','" + client.getPassword() + "','" + client.getMoney() + "')");
-        stmt.close();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO bank_client(name, password, money) VALUES (?, ?, ?)");
+        preparedStatement.setString(1, client.getName());
+        preparedStatement.setString(2, client.getPassword());
+        preparedStatement.setLong(3, client.getMoney());
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
     }
 
     public void createTable() throws SQLException {
-        Statement stmt = connection.createStatement();
-        stmt.execute("create table if not exists bank_client (id bigint auto_increment, name varchar(256), password varchar(256), money bigint, primary key (id))");
-        stmt.close();
+        PreparedStatement preparedStatement = connection.prepareStatement("create table if not exists bank_client (id bigint auto_increment, name varchar(256), password varchar(256), money bigint, primary key (id))");
+        preparedStatement.execute();
+        preparedStatement.close();
     }
 
     public void dropTable() throws SQLException {
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS bank_client");
-        stmt.close();
+        PreparedStatement preparedStatement = connection.prepareStatement("DROP TABLE IF EXISTS bank_client");
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
     }
 }
